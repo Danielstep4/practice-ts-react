28967039ba25ae2f3f69a6047e15241931b36f9d
Danielstep4 <63352307+Danielstep4@users.noreply.github.com>
2021-05-07 12:22:53 +0300

    Added Navbar DEMO

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,11 @@
 import React from "react";
-import Container from "@material-ui/core/Container";
-import Typography from "@material-ui/core/Typography";
-import Box from "@material-ui/core/Box";
-import Link from "@material-ui/core/Link";
-import ProTip from "./ProTip";
-
-const Copyright: React.FC = () => {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {"Copyright © "}
-      <Link color="inherit" href="https://material-ui.com/">
-        Your Website
-      </Link>{" "}
-      {new Date().getFullYear()}
-      {"."}
-    </Typography>
-  );
-};
+import HomePage from "./Components/HomePage";
 
 const App: React.FC = () => {
   return (
-    <Container maxWidth="sm">
-      <Box my={4}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          Create React App v4-beta example
-        </Typography>
-        <ProTip />
-        <Copyright />
-      </Box>
-    </Container>
+    <>
+      <HomePage />
+    </>
   );
 };
 export default App;
